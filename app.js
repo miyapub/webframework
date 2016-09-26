08b3c62a22d5e4cd3a680e05104900b6908532d1
miyapub <miyapub@outlook.com>
2016-09-26 10:27:59 +0800

    up

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var app = require('./lib/webframework');
+var fs = require('fs');
 
-app.get('/',function(){
-    
+app.get('/', function () {
+
     return 'index';
 });
 
-app.get('/about',function(req,res){
-    console.log('query',req.query);
-    res.end('about'+req.query);
+app.get('/about', function (req, res) {
+    res.end('about ' + req.query.name);
 });
 
-app.get('/foo/:bar',function(req,res){
-    var value=req.params.bar;
+app.get('/foo/:bar', function (req, res) {
+    var value = req.params.bar;
     res.end(value);
+});
+
+app.on404(function (req, res) {
+    fs.readFile('./html/404.html', function (err, data) {
+        if (err) throw err;
+        res.end(data);
+    });
 });
 
 app.start(3000);
